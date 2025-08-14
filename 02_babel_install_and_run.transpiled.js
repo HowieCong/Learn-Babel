96d52db79f1b8e6a38432d12980ccb3d6982be9b
hezc7 <howiecong@163.com>
2025-08-14 10:43:16 +0800

    feat: Chinese and English annotations 02_babel_install_and_run.transpiled.js

--- 02_babel_install_and_run.transpiled.js
+++ 02_babel_install_and_run.transpiled.js
@@ -1,7 +1,14 @@
 "use strict";
 
+// This is the code transpiled by Babel, compatible with legacy environments
 // 这是 Babel 转译后的代码，兼容老旧环境
+
 var greet = function greet(name) {
+  // Return a greeting string
+  // 返回问候字符串
   return "Hello, ".concat(name, "!");
 };
-console.log(greet('Babel'));
+
+// Output greeting to console
+// 在控制台输出问候语
+console.log(greet('Babel')); 
